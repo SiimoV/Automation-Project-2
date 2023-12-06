498bfe89f51d18c9b45b95b9ca892ce0088f2dfe
SiimoV <152617180+SiimoV@users.noreply.github.com>
2023-12-06 21:56:48 +0200

    Initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://jira.ivorreic.com/project/board',
+    baseUrl: "https://jira.ivorreic.com/project/board",
     env: {
-      baseUrl: 'https://jira.ivorreic.com/',
+      baseUrl: "https://jira.ivorreic.com/",
     },
-    defaultCommandTimeout: 30000,
-    requestTimeout: 20000,
+    defaultCommandTimeout: 30000, // oli 30000
+    requestTimeout: 20000, // oli 20000
+    // viewportWidth: 1000, // Set your desired width
+    viewportHeight: 1100, // Set your desired height
   },
 });
